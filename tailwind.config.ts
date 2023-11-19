d331e9de452a627d11755a476283eae0d14e8076
yuns <yuns.xie@qq.com>
2023-11-19 09:54:05 +0800

    ♻️ chore: drop useless comment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from 'tailwindcss'
 
-/** @type {import('tailwindcss').Config} */
 const config: Config = {
   content: ['./src/**/*.{ts,tsx,mdx}', './docs/**/*.{ts,tsx,mdx}'],
   theme: {
